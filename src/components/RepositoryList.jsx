b53990e0527eb5300658065d18dfff30fc815f80
Gabriel <gabriel.devloper@gmail.com>
2021-03-09 19:26:32 -0300

    feat: estado do componente (hook useState)

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,3 +1,4 @@
+
 import { RepositoryItem } from "./RepositoryItem";
 
 const repository = {
@@ -8,10 +9,11 @@ const repository = {
 
 
 export function RepositoryList(){
-
   
+        
     return(
         <section className="repository-list">
+        
             <h1>Lista de repositorios</h1>
             <ul>
                 <RepositoryItem repository={repository}/>
